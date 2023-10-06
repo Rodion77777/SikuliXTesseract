0222ee9a7b4f00437785382e8195e899cd4991cc
Rodion77777 <rodionchik17@gmail.com>
2023-10-07 02:28:25 +0300

    created file with variables Info.class

---
+++ src/main/java/org/example/Info.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Info {
+    public static final String pathTestImage = "src/main/resources/";
+    public static final String nameTestImageFile = "testImage.jpg";
+    public static final String pathToResultFile = "src/main/resources/";
+}
