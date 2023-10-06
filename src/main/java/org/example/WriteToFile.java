08eefba12ba31d1d69a769758365b6b134b636f7
Rodion77777 <rodionchik17@gmail.com>
2023-10-07 02:32:58 +0300

    created file writer

---
+++ src/main/java/org/example/WriteToFile.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class WriteToFile {
+    public static void inTXT (String text, String pathToFile) {
+        try {
+            FileUtils.writeStringToFile(new File(pathToFile), text, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
