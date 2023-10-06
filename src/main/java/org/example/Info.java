8dda764b206b3d567f79692e3a4e4e5331830737
Rodion77777 <rodionchik17@gmail.com>
2023-10-07 02:47:50 +0300

    added new function "getTimeStamp()"

--- src/main/java/org/example/Info.java
+++ src/main/java/org/example/Info.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Info {
     public static final String pathTestImage = "src/main/resources/";
     public static final String nameTestImageFile = "testImage.jpg";
     public static final String pathToResultFile = "src/main/resources/";
+
+    public static String getTimeStamp () {
+        return new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss")
+                .format(new Date());
+    }
 }
