793c1d19485debd5cf2a3fa06456ead4296cb37f
Rodion77777 <rodionchik17@gmail.com>
2023-10-07 03:46:53 +0300

    added new parameters for tesseract

--- src/main/java/org/example/util/Recognizer.java
+++ src/main/java/org/example/util/Recognizer.java
@@ -4,11 +4,15 @@ import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
 
 import java.io.File;
+import java.util.Arrays;
 
 public class Recognizer {
     public static String getText (String pathToImage) {
 
         Tesseract tesseract = new Tesseract();
+        tesseract.setDatapath(Info.pathToTesseractOCR);
+        tesseract.setLanguage("rus");
+        tesseract.setTessVariable("tessedit_char_whitelist", "0123456789");
 
         String result = null;
 
