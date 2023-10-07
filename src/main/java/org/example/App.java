cee44c289c109bb7adceb0a27b215ce0b63221f6
Rodion77777 <rodionchik17@gmail.com>
2023-10-08 02:34:54 +0300

    update App.class. Changed main method and wil be added new function for pattern

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,17 +4,44 @@ import org.example.util.Info;
 import org.example.util.Recognizer;
 import org.example.util.WriteToFile;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
- * Hello world!
+ *
  *
  */
 public class App {
     public static void main (String[] args) {
 
-        WriteToFile.inTXT
-        (
-            Recognizer.getText(Info.pathTestImage + Info.nameTestImageFile2),
-            Info.pathToResultFile
-        );
+        String[] images = new String[]
+                {
+                        Info.nameTestImageFile,
+                        Info.nameTestImageFile2,
+                        Info.nameTestImageFile3,
+                        Info.nameTestImageFile4
+                };
+
+        for (String s : images) {
+            String result = Recognizer.getText(Info.pathTestImage + s, Info.DPI);
+            result = viaPattern(result);
+            WriteToFile.inTXT(result, Info.pathToResultFile);
+        }
+    }
+
+    public static String viaPattern (String resultText) {
+
+        Pattern pattern = Pattern.compile("\\s(\\d{5})");
+        Matcher matcher = pattern.matcher(" " + resultText);
+        String foundDigits = null;
+
+        if (matcher.find()) {
+            foundDigits = matcher.group(1);
+            System.out.println("[info] Распознанный пароль: " + foundDigits);
+        } else {
+            System.out.println("[info] Совпадение не найдено");
+        }
+
+        return foundDigits;
     }
 }
