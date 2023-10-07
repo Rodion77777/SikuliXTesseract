fa594c3a5bc044554baedf55f0afc79c7a2928e8
Rodion77777 <rodionchik17@gmail.com>
2023-10-08 02:30:47 +0300

    update Info.class (added path to new test images)

--- src/main/java/org/example/util/Info.java
+++ src/main/java/org/example/util/Info.java
@@ -4,9 +4,13 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Info {
+
+    public static final String DPI = "87";
     public static final String pathTestImage = "src/main/resources/";
     public static final String nameTestImageFile = "testImage.jpg";
     public static final String nameTestImageFile2 = "testImage2.png";
+    public static final String nameTestImageFile3 = "testImage3.png";
+    public static final String nameTestImageFile4 = "testImage4.png";
     public static final String pathToResultFile = "src/main/resources/";
     public static final String pathToTesseractOCR = "C:/Users/Admin/TesseractOCR/tesseract-main/";
 
