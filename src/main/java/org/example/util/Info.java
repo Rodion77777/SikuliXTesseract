32f15f4ee2d4d77ef0a169a8d8e22aeb08d7d846
Rodion77777 <rodionchik17@gmail.com>
2023-10-07 03:46:34 +0300

    update - added new variable with path to test file png

--- src/main/java/org/example/util/Info.java
+++ src/main/java/org/example/util/Info.java
@@ -6,7 +6,9 @@ import java.util.Date;
 public class Info {
     public static final String pathTestImage = "src/main/resources/";
     public static final String nameTestImageFile = "testImage.jpg";
+    public static final String nameTestImageFile2 = "testImage2.png";
     public static final String pathToResultFile = "src/main/resources/";
+    public static final String pathToTesseractOCR = "C:/Users/Admin/TesseractOCR/tesseract-main/";
 
     public static String getTimeStamp () {
         return new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss")
