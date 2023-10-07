5d13b264cd02ac5f9036a750e19f25722bf5d527
Rodion77777 <rodionchik17@gmail.com>
2023-10-07 03:47:05 +0300

    update

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,20 @@
 package org.example;
 
+import org.example.util.Info;
+import org.example.util.Recognizer;
+import org.example.util.WriteToFile;
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main (String[] args) {
-        System.out.println ("Hello World!");
+
+        WriteToFile.inTXT
+        (
+            Recognizer.getText(Info.pathTestImage + Info.nameTestImageFile2),
+            Info.pathToResultFile
+        );
     }
 }
